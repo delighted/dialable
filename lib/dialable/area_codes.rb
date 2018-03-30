80c8307ad34bb5e3251acd1f98770129a6f240b5
Mark Dodwell <markdodwell@gmail.com>
2018-03-30 14:16:12 -0700

    sidestep rubygems bug. see: https://github.com/rubygems/rubygems/issues/1673

--- lib/dialable/area_codes.rb
+++ lib/dialable/area_codes.rb
@@ -4,18 +4,7 @@ module Dialable
   module AreaCodes
 
     def self.datadir
-      # If we are in the source directory, don't use the datadir from the gem.
-      datadir = if File.identical?(ENV['PWD'], File.join(File.dirname(__FILE__), '..', '..'))
-                  File.join(File.dirname(__FILE__), '..', '..', 'data', 'dialable')
-                else
-                  Gem::Specification.find_by_name('dialable').datadir
-                end
-
-      if ! File.directory?(datadir)
-        fail "Can't find the datadir provided by the gem: #{Gem.datadir('dialable')} or by the source: #{File.join(File.dirname(__FILE__), '..', 'data', 'dialable')}."
-      end
-
-      datadir
+      File.join(File.dirname(__FILE__), '..', '..', 'data', 'dialable')
     end
 
     # Valid area codes per nanpa.com
